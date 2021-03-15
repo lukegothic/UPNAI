e26a0c4fb9f5edb5546254593eb8435bfc2de3ab
Luke <lukegothic@gmail.com>
2021-03-15 21:17:29 +0100

    Clase 4 INICIO - Tarea sentido oido

---
+++ Assets/Noise.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise
+{
+    public Vector3 sourcePosition;
+    public float intensity;
+    public string noiseType;
+
+    public Noise(Vector3 sourcePosition, float intensity, string noiseType) {
+        this.sourcePosition = sourcePosition;
+        this.intensity = intensity;
+        this.noiseType = noiseType;
+    }
+}
