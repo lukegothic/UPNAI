a272ff063338ff97f410e41d55e7b3e92294a817
Luke <lukegothic@gmail.com>
2021-04-28 23:08:58 +0200

    CLASE9 FINAL GOAPOK

---
+++ Assets/GOAP/Actions/VenderCorazonAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VenderCorazonAction : GOAPAction
+{
+   public VenderCorazonAction() : base()
+    {
+        Preconditions.Add(new GOAPState("has_venta", false));
+        Preconditions.Add(new GOAPState("has_corazon", true));
+        Effects.Add(new GOAPState("has_venta", true));
+        Effects.Add(new GOAPState("has_corazon", false));
+    }
+}
