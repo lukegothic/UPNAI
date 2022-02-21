1baa2c091312cb3ebe9fdb6f65a36e62a52323fc
Iván Pérez <lukegothic@gmail.com>
2022-02-21 17:12:16 +0100

    clase4 with fixes

--- Assets/Noise.cs
+++ Assets/Noise.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using Ludiq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Noise
 {
-    public Vector3 sourcePosition;
+    public Vector3 position;
     public float intensity;
-    public string noiseType;
+    public string type;
+    public float lifetime;
 
-    public Noise(Vector3 sourcePosition, float intensity, string noiseType) {
-        this.sourcePosition = sourcePosition;
+    public Noise(Vector3 position, float intensity, string type, float lifetime) {
+        this.position = position;
         this.intensity = intensity;
-        this.noiseType = noiseType;
+        this.type = type;
+        this.lifetime = lifetime;
     }
 }
