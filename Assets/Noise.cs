4835a3d3baea869170ad21a0a2a2f8d7f11de692
Iván Pérez <lukegothic@gmail.com>
2022-02-22 11:48:25 +0100

    final final

--- Assets/Noise.cs
+++ Assets/Noise.cs
@@ -1,7 +1,10 @@
 ﻿using Ludiq;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Noise
 {
@@ -16,4 +19,27 @@ public class Noise
         this.type = type;
         this.lifetime = lifetime;
     }
+    /*
+    [CustomPropertyDrawer(typeof(Noise))]
+    public class NoiseDrawerUIE : PropertyDrawer {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property) {
+            // Create property container element.
+            var container = new VisualElement();
+
+            // Create property fields.
+            var position = new PropertyField(property.FindPropertyRelative("position"));
+            var intensity = new PropertyField(property.FindPropertyRelative("intensity"));
+            var type = new PropertyField(property.FindPropertyRelative("type"), "Fancy Name");
+            var lifetime = new PropertyField(property.FindPropertyRelative("lifetime"), "Fancy Name 2");
+
+            // Add fields to the container.
+            container.Add(position);
+            container.Add(intensity);
+            container.Add(type);
+            container.Add(lifetime);
+
+            return container;
+        }
+    }
+    */
 }
