49db18a0385dfd93854b9bcba4475cddbb5d040e
Iván Pérez <lukegothic@gmail.com>
2022-03-27 23:36:30 +0200

    inicio clase 9 GOAP

---
+++ Assets/Scripts/GOAP/Actions/CogerFlorAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogerFlorAction : GOAPAction
+{
+   public CogerFlorAction() : base()
+    {
+        Preconditions.Add(new GOAPState("has_flor", false));
+        Effects.Add(new GOAPState("has_flor", true));
+    }
+}
